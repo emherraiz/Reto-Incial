fe0c7b53c37b64a6822a6d6ce1801d95751986cb
Enrique Marina <eherraiz@myuax.com>
2022-09-09 18:27:49 +0200

    er

---
+++ lanzador.py
@@ -0,0 +1,14 @@
+from Tipos_de_vehiculos.Vehiculo import Vehiculo
+from Tipos_de_vehiculos.Coche import Coche
+from Tipos_de_vehiculos.Bicicleta import Bicicleta
+from Tipos_de_vehiculos.Motocicleta import Motocicleta
+from Tipos_de_vehiculos.Camioneta import Camioneta
+from Catalogar import catalogar
+
+
+Mercedes = Coche('Azul', 4, 180, 25)
+Fragoneta = Camioneta('Verde', 6, 100, 20, 190)
+BMX = Bicicleta('Blanco', 2, 'Urbana')
+KTM = Motocicleta('Amarillo', 2, 'Urbana', 125, 20)
+
+lista = [Mercedes, Fragoneta, BMX, KTM]
